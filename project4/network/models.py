c9ed5fec140f7bce7843609415bf9b7da321d059
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-03-26 09:05:37 +0200

    created profile model and modified setting,py

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    username = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    bio = models.TextField()
+    
+    def __str__(self):
+        return f"{self.username} by {self.user.username}"
+
--- project4/project4/settings.py
+++ project4/project4/settings.py
@@ -115,6 +115,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 #used for aploading images 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
