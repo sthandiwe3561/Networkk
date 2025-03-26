af6e679dd24bd096a45ddb4536080b2ec30da456
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2025-03-26 08:51:34 +0200

    modified create_profile,setting.py and url.py

--- project4/project4/settings.py
+++ project4/project4/settings.py
@@ -115,6 +115,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+#used for aploading images 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
